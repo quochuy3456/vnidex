384add32e1dffe858bb75f47a9093e0e53008eab
quochuy3456 <quochuy3456@gmail.com>
2020-07-01 23:26:27 +0700

    add gif

--- vnidxUI/views.py
+++ vnidxUI/views.py
@@ -19,7 +19,8 @@ def bank(request):
     else:
         lst_lai_suat = lai_suat_input.split(",")[0:len(lst_input)]
 
-    lst_re = [['Cpn Code', 'Chỉ số EPS', 'Tổng tài sản', 'KLDLH', 'Tốc độ tăng trưởng', 'Gía hiện tại', 'Lãi suất', 'Giá trị thực 1', 'Gía trị thực 2', 'Gía trị thực 3', 'Kết luận']]
+    lst_re = [['Mã công ty', 'Chỉ số EPS', 'Tổng tài sản', 'KLDLH', 'Tốc độ tăng trưởng', 
+                'Giá hiện tại', 'Lãi suất', 'Giá trị thực 1', 'Giá trị thực 2', 'Giá trị thực 3', 'Kết luận']]
     for bk in range(len(lst_input)):
         crawler = CrawlerBank(lst_input[bk].strip())
         value1 = crawler.get_real_value_1()
@@ -34,6 +35,7 @@ def bank(request):
         result = "Có thể mua" if current_price < value3 else "Nên xem xét"
         summ = [lst_input[bk].upper(), EPS, capital, KLDLH, rising_speed, current_price, lai_suat, value1, value2, value3, result]
         lst_re.append(summ)
+    lst_re = list(zip(*lst_re))
     return render(request, 'vnidxUI/index.html', {'data':lst_re, 'cpn_code': cpn_code, 'lai_suat': lai_suat_input})
 
 def footanddrink(request):
@@ -49,8 +51,8 @@ def footanddrink(request):
     else:
         lst_lai_suat = lai_suat_input.split(",")[0:len(lst_input)]
 
-    lst_re = [['Cpn Code', 'Chỉ số EPS', 'Tổng tài sản', 'KLDLH', 'Tốc độ tăng trưởng', 'Gía hiện tại', 'Lãi suất',
-               'Giá trị thực 1', 'Gía trị thực 2', 'Gía trị thực 3', 'Kết luận']]
+    lst_re = [['Mã công ty', 'Chỉ số EPS', 'Tổng tài sản', 'KLDLH', 'Tốc độ tăng trưởng', 'Giá hiện tại', 'Lãi suất',
+               'Giá trị thực 1', 'Giá trị thực 2', 'Giá trị thực 3', 'Kết luận']]
     for bk in range(len(lst_input)):
         crawler = CrawlerFoodandDrink(lst_input[bk].strip())
         value1 = crawler.get_real_value_1()
